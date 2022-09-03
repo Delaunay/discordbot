03723d1a4404a6eb5f5efd0f9466462f4f09fca9
Setepenre <pierre.delaunay.tr@gmail.com>
2022-09-03 15:20:54 -0400

    Update main.py

--- vlcbot/core/main.py
+++ vlcbot/core/main.py
@@ -6,10 +6,7 @@ from vlcbot.core.bot import VLCBotClient
 from vlcbot.core.sources import get_source
 
 
-TOKEN = 'MTAxNTY2Mzc2MTE2OTg1MDQxOA.GJ22GE.iAMOHuP1WCXC780rfDUv99CoOT2OfZ0qv1Ov1A'
-os.environ['DISCORD_TOKEN'] = TOKEN
-
-
+TOKEN = os.environ.get('DISCORD_TOKEN')
 
 # get_source('vlcbot.plugins.sources.file')
 
